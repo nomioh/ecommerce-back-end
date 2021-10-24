48bc88184daa5cb69af2f3c85744b6ef45143b0c
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-10-24 16:05:03 -0400

    define columns for product

--- models/Product.js
+++ models/Product.js
@@ -16,6 +16,33 @@ Product.init(
       primaryKey: true,
       autoIncrement: True,
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
